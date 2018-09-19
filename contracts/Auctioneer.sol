baea04c98bae59ad1e2c4f621d95e4dfd62468c5
vaibhavb26 <vaibhav.bajaj@research.iiit.ac.in>
2018-09-19 12:16:45 +0530

    Add Register Notary function

--- contracts/Auctioneer.sol
+++ contracts/Auctioneer.sol
@@ -5,8 +5,9 @@ contract Auctioneer{
     /* this variable holds the address of the moderator which will run the bidding */
     address public moderator;
 
-    /* m is the number of distinct items and q is a large prime number */
-    uint public m, q;
+    /* m is the set of all distinct items and q is a large prime number */
+    uint[] public m;
+    uint public q;
 
     /* we will use unix timestamps to keep account of time*/
     /* Till this time all bidders who want to take part should be registered */
@@ -16,20 +17,29 @@ contract Auctioneer{
     uint num_bidders;
 
     /* To store all the winners among the Bidders */
-    address public memory Winners = new uint[];
+    address[] public Winners;
 
     /* This structure will represent each bidder in the game */
     struct Bidder{
         address account;
         /* For storing the item choices of a particular bidder */
         /* Two arrays of variable size in the memory */
-        uint memory u = new uint[];
-        uint memory v = new uint[];
-        uint w_1, w_2;
+        uint[] u;
+        uint[] v;
+        uint w_1;
+        uint w_2;
     }
-
+    
     /* For now any number of Bidders are allowed */
     Bidder[] public bidders;
+    
+    /* This structure will represent each notary in the game */
+    struct Notary{
+        address account;
+    }
+    Notary[] public notaries;
+    mapping (address => uint) is_notary;
+    
 
     // ensures the call is made before certain time
     modifier onlyBefore(uint _time){
@@ -49,17 +59,33 @@ contract Auctioneer{
     // ensures one of the Bidder is calling the function
     modifier isBidder(){
         bool is_bidder = false;
-        for(uint i = 0;i < a.length; i++){
+        for(uint i = 0;i < bidders.length; i++){
             if(msg.sender == bidders[i].account)
                 is_bidder = true;
         }
-        require(is_bidder, "You are not part of this auction"); _;
+        require(is_bidder == true, "You are not part of this auction"); _;
     }
     
-    constructor(int num_items, int _q, uint inputTime){
-        inputDeadline = inputTime;
+    /* You can call this event to check what all items are available for auction */
+    event displayItems(uint[] m);
+    
+    /* 27/06/2019 04:00:00 -> unix time stamp = 1561608000 */
+    constructor (uint[] _items, uint _q, uint _inputTime) public{
+        inputDeadline = _inputTime;
         moderator = msg.sender;
-        m = num_items;
+        m = _items;
         q = _q;
+    }
+    
+    /* A public function which will register the notary iff one has not registered before */
+    function registerNotary()
+    public
+    
+    // allow registration of notaries only before the inputDeadline
+    onlyBefore(inputDeadline)
+    {
+        require(is_notary[msg.sender] == 0, "Sorry, but you have already registered for this auction");
+        is_notary[msg.sender] = 1;   // Means now this is present
+        notaries.push(Notary({account:msg.sender}));
     }
 }
