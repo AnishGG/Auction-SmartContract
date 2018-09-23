b9d5ec51b05b15082ab7778fd153e194d4103685
AnishGulati <anish.gulati@research.iiit.ac.in>
2018-09-23 23:47:44 +0530

    Add 3 more tests

--- test/auctioneer.js
+++ test/auctioneer.js
@@ -34,7 +34,8 @@ contract('Auctioneer',  (accounts) => {
 			assert.equal(prevcnt.c[0] + 1, newcnt.c[0], 'Bidder is not registered');
 		})   
 	}
-	it('Check if two notaries with sample address can get registered', async() => {
+
+	it('Check if two notaries with same address can get registered', async() => {
 		var prevcnt = await contractInstance.getNotarycnt();
 		try {
 			await contractInstance.registerNotary({from: p[0]});
@@ -46,7 +47,7 @@ contract('Auctioneer',  (accounts) => {
 		assert.equal(prevcnt.c[0], newcnt.c[0], 'Bidder is not registered');
 	})
 
-	it('Check if two bidders with sample address can get registered', async() => {
+	it('Check if two bidders with same address can get registered', async() => {
 		var prevcnt = await contractInstance.getBiddercnt();
 		try {
 			await contractInstance.registerBidder([2,3],[18,18], 5, 15, {from: p[1]});
@@ -55,6 +56,46 @@ contract('Auctioneer',  (accounts) => {
 		
 		}
 		var newcnt = await contractInstance.getBiddercnt();
-		assert.equal(prevcnt.c[0], newcnt.c[0], 'Bidder is not registered');
-	})   
+		assert.equal(prevcnt.c[0], newcnt.c[0], 'Bidder is registered but it should not be');
+	})
+
+	it('Check if Bidders items are valid',  async() => {
+		var prevcnt = await contractInstance.getBiddercnt();
+		try {
+			await contractInstance.registerBidder([18,3],[18,18], 5, 15, {from: p[8]});
+			assert.ok(false, 'The contract should throw an exception to \
+				as the bidder items are not valid')
+		}
+		catch(err){
+			console.log(err.message)
+			assert.ok(true, 'The contract is throwing an error as the \
+				bidder items are not valid')
+		}
+	})
+
+	it('Check if Bidder gets added even when notary is not assigned to bidder',  async() => {
+		/* Every notary is assigned before this test */
+		try {
+			await contractInstance.registerBidder([3,3],[18,18], 5, 15, {from: p[8]});
+			assert.ok(false, 'The contract should throw an exception to \
+				as the bidder items are not valid')
+		}
+		catch(err){
+			console.log(err.message)
+			assert.ok(true, 'The contract is throwing an error as the \
+				bidder is not registered because notary was not available')
+		}
+	})
+
+	it('Check if the assigned notary of bidder is not bidder himself',  async() => {
+		flag = true;
+		for(i = 1; i < 8; i+=2) {
+			var x = await contractInstance.getAssignedNotary(p[i]);
+			// console.log(x);
+			if(x == p[i])
+				flag = false;
+		}
+		assert.equal(flag, true, 'Bidder is Notary of himself')
+	})
+
 })
